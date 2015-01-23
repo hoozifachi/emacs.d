08fbcafa56c7f9ab79b5cea6f3f063e8dff5bb68
Jason Sweet <hoozifachi@gmail.com>
2015-01-22 23:58:46 -0600

    Set javascript indent level.

--- init.el
+++ init.el
@@ -76,7 +76,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (tango-dark)))
- '(yas-prompt-functions (quote (yas-ido-prompt yas-dropdown-prompt yas-completing-prompt yas-x-prompt yas-no-prompt))))
+ '(js-indent-level 2)
+ '(yas-prompt-functions
+   (quote
+    (yas-ido-prompt yas-dropdown-prompt yas-completing-prompt yas-x-prompt yas-no-prompt))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
